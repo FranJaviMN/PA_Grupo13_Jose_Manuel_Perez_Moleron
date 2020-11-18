f8cfb97199b9ad35e3bc7777adbbb5e6593fb63b
Jose Manuel Perez Moleron <josemanuelpmoleron@gmail.com>
2020-11-18 19:09:01 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Titulo</title>
+    </head>
+    <body>
+        <?php
+            echo "Esto es un proyecto de prueba para la EPD7";
+        ?>
+    </body>
+</html>
+
